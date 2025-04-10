951fbe45a55d0cd847b4f13b0d075223c9795696
Buck Melton <buckm@alumni.stanford.edu>
2025-04-10 10:40:00 -0700

    Toggle edit/save state of player edit input

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,18 +3,18 @@ import { useState } from 'react';
 export default function Player({name, symbol}) {
   const [isEditing, setIsEditing] = useState(false);
 
-  const handleClick = () => {
-    setIsEditing(true);
+  const handleEditClick = () => {
+    setIsEditing(editing => !editing);
   }
 
   return (
     <li>
       <span className="player">
         {!isEditing && <span className="player-name">{name}</span>}
-        {isEditing && <input type="text" />}
+        {isEditing && <input type="text" required value={name}/>}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={handleClick}>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit'}</button>
     </li>
   );
 }
