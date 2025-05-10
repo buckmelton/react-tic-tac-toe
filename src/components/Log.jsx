18f6a1f64a4f8c049da2e7a38371c18b390bdc86
Buck Melton <buckm@alumni.stanford.edu>
2025-05-09 20:22:04 -0700

    Correct log (move list) keys

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,8 +1,8 @@
 export default function Log({ turns }) {
 
   return <ol id="log">
-    {turns.map((turn) => 
-      <li>Player {turn.player}: Row {turn.square.row}, Col {turn.square.col}</li>
+    {turns.map(turn => <li key={`${turn.square.row}${turn.square.col}`}>
+      Player {turn.player} selected Row {turn.square.row}, Col {turn.square.col}</li>
     )}
   </ol>
 }
