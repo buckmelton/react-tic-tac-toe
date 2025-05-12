45d5f170774116baeea6eb20555027b599835053
Buck Melton <buckm@alumni.stanford.edu>
2025-05-11 21:06:04 -0700

    Display player name for winner

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,10 @@ function deriveActivePlayer(gameTurns) {
 }
 
 function App() {
+  const [players, setPlayers] = useState({
+    'X': 'Player 1',
+    'O': 'Player 2'
+  });
   const [gameTurns, setGameTurns] = useState([]);
   const activePlayer = deriveActivePlayer(gameTurns);
 
@@ -57,7 +61,7 @@ function App() {
     if (firstSquareSymbol &&
         firstSquareSymbol === secondSquareSymbol &&
         firstSquareSymbol === thirdSquareSymbol) {
-      winner = firstSquareSymbol;
+      winner = players[firstSquareSymbol];
     }
   } 
 
@@ -67,12 +71,30 @@ function App() {
     setGameTurns([]);
   }
 
+  function handlePlayerNameChange(playerSymbol, playerName) {
+    setPlayers(prevPlayers => {
+      return {
+        ...players,
+        [playerSymbol]: playerName
+      }
+    });
+  }
+
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'}/>
-          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === 'X'}
+            onChangeName={handlePlayerNameChange}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === 'O'}
+            onChangeName={handlePlayerNameChange}/>
         </ol>
         {(winner || isDraw) && <GameOver winner={winner} onRestart={handleRestart} />}
         <GameBoard
